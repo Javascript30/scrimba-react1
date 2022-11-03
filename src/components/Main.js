e0ea4688232c9fcb8e1f6bf89bf26c1ddd8190b8
Lesley <leskimuti@gmail.com>
2022-11-03 17:05:01 +0300

    Working on dark-light modes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
-function Main (){
-  return(
-    <main>
-      <h1 className='main--title'>Fun facts about React</h1>
-      <ul className='facts'>
+function Main(props) {
+  return (
+    <main className={props.darkMode ? "dark" : ""}>
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="facts">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub</li>
@@ -12,7 +12,7 @@ function Main (){
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
     </main>
-  )
+  );
 }
 
-export default Main
+export default Main;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import reactLogo from '../images/logo192.png'
+import React from "react";
+import reactLogo from "../images/logo192.png";
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
-    <nav className='nav'>
-      <div className='logo'>
-      <img src={reactLogo} alt='logo-img' width='29.93px'/ >
-      <h1>ReactFacts</h1>
+    <nav className={`nav ${props.darkMode ? "dark" : ""} `}>
+      <div className="logo">
+        <img src={reactLogo} alt="logo-img" width="29.93px" />
+        <h1>ReactFacts</h1>
       </div>
-      <h2 className='header'>React Course - Project 1</h2>
+
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={props.toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
     </nav>
-  )
+  );
 }
