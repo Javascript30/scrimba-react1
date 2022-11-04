f8472a4afdeb9cc7fc86139d53a3dc54d312952b
Lesley <leskimuti@gmail.com>
2022-11-04 12:48:14 +0300

    Used props to switch between modes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
 // import ReactDOM from 'react-dom'
-import Navbar from './components/Navbar'
-import Main from './components/Main'
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import { useState } from "react";
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
-    <div className='container'>
-      < Navbar />
-      <Main />
+    <div className="container">
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
-  )
+  );
 }
